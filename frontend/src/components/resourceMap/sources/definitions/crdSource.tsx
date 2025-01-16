f726ce6f47f658fa068f11ec320bc948385c3bfb
Alexander North <alexandernorth@users.noreply.github.com>
2025-01-16 09:59:33 +0100

    split crds into filter options, and load crs

--- frontend/src/components/resourceMap/sources/definitions/crdSource.tsx
+++ frontend/src/components/resourceMap/sources/definitions/crdSource.tsx
@@ -1,257 +1,43 @@
-import { Icon } from '@iconify/react';
 import { useMemo } from 'react';
 import CRD from '../../../../lib/k8s/crd';
-import { KubeObject } from '../../../../lib/k8s/KubeObject';
 import { useNamespaces } from '../../../../redux/filterSlice';
 import { GraphEdge, GraphSource } from '../../graph/graphModel';
-import { getKindGroupColor, KubeIcon } from '../../kubeIcon/KubeIcon';
+import { KubeIcon } from '../../kubeIcon/KubeIcon';
 import { makeKubeObjectNode } from '../GraphSources';
 
-/*
-function getCrdSources() {
-  let sources: GraphSource[] = [];
-  let testCrds = ["ipaddressclaims.netbox.dev", "ipaddresses.netbox.dev"];
-  for (const crd of testCrds) {
+export function crdSource(crds: CRD[]): GraphSource {
+  const sources: GraphSource[] = [];
+
+  for (const crd of crds) {
     sources.push({
-      id: 'crd-' + crd, //TODO: Change to dynamic
-      label: 'test',
+      id: 'cr-' + crd.getName(),
+      label: crd.getName(),
       icon: <KubeIcon kind="CustomResourceDefinition" />,
-      useData() {
-        const [crdResource, error] = CRD.useGet(crd);
-        if (error || crdResource == null) return null
-        const crClass = crdResource.makeCRClass();
-        console.log(crClass)
-
-        const crClassList = crClass.useList({ cluster: crdResource.cluster });
-        // console.log(crClassList)
-
+      useData: () => {
+        const crClass = crd.makeCRClass();
+        const [crInstances, error] = crClass.useList({
+          cluster: crd.cluster,
+          namespace: useNamespaces(),
+        });
         return useMemo(() => {
-          // if (!crClassList || !!error) {
-          //   return {
-          //     nodes: [],
-          //     edges: [],
-          //   };
-          // }
-
-          // const queries = dataClassCrds.map(it => it.data);
-          // const crs = queries.filter(crd => crd.items != null && crd.items.length > 0);
-
-          // const edges: GraphEdge[] = [];
-          // find used pvc
-          // pods.forEach(pod => {
-          //   pod.spec.volumes?.forEach(volume => {
-          //     if (volume.persistentVolumeClaim) {
-          //       const crd = crds.find(
-          //         crd => crd.metadata.name === volume.persistentVolumeClaim!.claimName
-          //       );
-          //       if (crd) {
-          //         edges.push(makeKubeToKubeEdge(crd, pod));
-          //       }
-          //     }
-          //   });
-          // });
-
+          if (!crInstances || !!error) {
+            console.error('error:', error);
+            return null;
+          }
+          const edges: GraphEdge[] = [];
           return {
-            nodes: [],
-            edges: [],
+            nodes: crInstances.map(makeKubeObjectNode) ?? [],
+            edges,
           };
-        }, []);
-
+        }, [crInstances, error]);
       },
     });
   }
-  return sources;
-}
 
-function getCRInstances () {
-  // const [crds, error] = CRD.useList( {fieldSelector: "metadata.name=ipaddressclaims.netbox.dev"});
-  // const crdClass = crd?.makeCRClass();
-  // const data = crdClass?.useList({ cluster: crd.cluster, namespace: useNamespaces() });
-  // const dataClassCrds = crds?.map(crd => {
-  //   return { data, crdClass, crd };
-  // });
-  // return dataClassCrds
-}
-
-function crdSources() {
-  let testCrds = ["ipaddressclaims.netbox.dev", "ipaddresses.netbox.dev"];
-  let sources: GraphSource[] = [];
-  testCrds.forEach(crdName => {
-    const [crd, _] = CRD.useGet(crdName);
-    if (crd == null) return;
-    const cr = crd.makeCRClass();
-    sources.push({
-      id: 'crd-' + "ipaddressclaims.netbox.dev", //TODO: Change to dynamic
-      label: 'test',
-      icon: <KubeIcon kind="CustomResourceDefinition" />,
-      useData() {
-        const crs = cr.useList({namespace: useNamespaces()})
-        console.log(crs)
-        return useMemo(() => {
-          if (!crs) return null;
-    
-          return {
-            nodes: [],
-            edges: [],
-          };
-        }, [crs]);
-      }
-    });
-  });
-  return sources;
-}
-
-const demoSource: GraphSource = {
-  id: 'crd-' + "ipaddressclaims.netbox.dev", //TODO: Change to dynamic
-  label: 'test',
-  icon: <KubeIcon kind="CustomResourceDefinition" />,
-  useData() {
-    const namespace = useNamespaces()
-    
-    const crs = cr?.useList({namespace: namespace})
-    
-    return useMemo(() => {
-      if (!crs) return null;
-  
-      // console.log(crClassList)
-      // if (!crClassList || !!error) {
-      //   return {
-      //     nodes: [],
-      //     edges: [],
-      //   };
-      // }
-
-      // const queries = dataClassCrds.map(it => it.data);
-      // const crs = queries.filter(crd => crd.items != null && crd.items.length > 0);
-
-      // const edges: GraphEdge[] = [];
-      // find used pvc
-      // pods.forEach(pod => {
-      //   pod.spec.volumes?.forEach(volume => {
-      //     if (volume.persistentVolumeClaim) {
-      //       const crd = crds.find(
-      //         crd => crd.metadata.name === volume.persistentVolumeClaim!.claimName
-      //       );
-      //       if (crd) {
-      //         edges.push(makeKubeToKubeEdge(crd, pod));
-      //       }
-      //     }
-      //   });
-      // });
-
-      return {
-        nodes: [],
-        edges: [],
-      };
-    }, [crs]);
-  }
-};
-
-function createSource(crd): GraphSource {
   return {
-    id: 'crd-' + "ipaddressclaims.netbox.dev", //TODO: Change to dynamic
-    label: 'test',
+    id: 'crs',
+    label: 'CRs',
     icon: <KubeIcon kind="CustomResourceDefinition" />,
-    useData() {
-      const crs = crd.useList({namespace: useNamespaces()})
-      
-      return useMemo(() => {
-        if (!crs) return null;
-  
-  
-        return {
-          nodes: [],
-          edges: [],
-        };
-      }, [crs]);
-    }
+    sources: sources,
+  };
 }
-
-function iterateCRDs() {
-  const [crdResource, error] = CRD.useGet(crd);
-  const crClass = crdResource.makeCRClass();
-  return [createSource(crClass)]
-}*/
-
-const createCrdSources = (crds: CRD[]): GraphSource[] => {
-  console.log('createCrdSources');
-  console.log(crds);
-  const crdGraphSource: GraphSource = {
-    id: 'crd', //+ crd.metadata.name,
-    label: 'crd', //crd.metadata.name,
-    icon: <KubeIcon kind="CustomResourceDefinition" />,
-    useData: () => {
-      const edges: GraphEdge[] = [];
-      return {
-        nodes: crds.map(makeKubeObjectNode) ?? [],
-        edges,
-      };
-    },
-  };
-  return [crdGraphSource];
-};
-
-const createCrSources = (crds: CRD[]): GraphSource[] => {
-  const dataClassCrds = crds.map(crd => {
-    const crdClass = crd.makeCRClass();
-    const data = crdClass.useList({ cluster: crd.cluster, namespace: useNamespaces() });
-    return { data, crdClass, crd };
-  });
-
-  const queries = dataClassCrds.map(it => it.data);
-
-  //const [isWarningClosed, setIsWarningClosed] = useState(false);
-
-  //const { crInstancesList, getCRDForCR, isLoading, crdsFailedToLoad, allFailed } = useMemo(() => {
-  const { crInstancesList } = useMemo(() => {
-    const isLoading = queries.some(it => it.isLoading || it.isFetching);
-
-    // Collect the names of CRD that failed to load lists
-    const crdsFailedToLoad: string[] = [];
-    queries.forEach((it, i) => {
-      if (it.isError) {
-        crdsFailedToLoad.push(crds[i].metadata.name);
-      }
-    });
-
-    // Create a map to be able to link to CRD by CR kind
-    const crKindToCRDMap = queries.reduce((acc, { items }, index) => {
-      if (items?.[0]) {
-        acc[items[0].kind] = crds[index];
-      }
-      return acc;
-    }, {} as Record<string, CRD>);
-    const getCRDForCR = (cr: KubeObject) => crKindToCRDMap[cr.kind];
-
-    return {
-      crInstancesList: queries.flatMap(it => it.items ?? []),
-      getCRDForCR,
-      isLoading,
-      crdsFailedToLoad,
-      allFailed: crdsFailedToLoad.length === queries.length,
-    };
-  }, queries);
-  console.log('crInstancesList');
-  console.log(crInstancesList);
-  const crGraphSource: GraphSource = {
-    id: 'cr-', //+ crd.metadata.name,
-    label: 'cr', //crd.metadata.name,
-    icon: <KubeIcon kind="CustomResourceDefinition" />,
-    useData: () => {
-      const edges: GraphEdge[] = [];
-      return {
-        nodes: crInstancesList.map(makeKubeObjectNode) ?? [],
-        edges,
-      };
-    },
-  };
-  return [crGraphSource];
-};
-
-export const crdSource = (crds: CRD[]): GraphSource => ({
-  id: 'crs',
-  label: 'CRs',
-  icon: <Icon icon="mdi:puzzle" width="100%" height="100%" color={getKindGroupColor('other')} />,
-  // sources: getCrdSources(),
-  sources: [...createCrdSources(crds), ...createCrSources(crds)], //iterateCRDs(),
-});
